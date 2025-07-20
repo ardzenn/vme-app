41ccc7f497d5dfcfcfa074551b8e6c3b97b5bf1f
Ardon <ardongadgarcia@gmail.com>
2025-07-21 06:55:01 +0800

    changed something in app.js

--- api/app.js
+++ api/app.js
@@ -77,4 +77,4 @@ io.on('connection', (socket) => {
 });
 
 app.get('/', (req, res) => res.redirect('/login')); 
-server.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+module.exports = app;
