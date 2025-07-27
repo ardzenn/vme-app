17aff612391ed02b2c78937b7a97efba1800ad5a
Ardon <ardongadgarcia@gmail.com>
2025-07-27 12:30:16 +0800

    added notification update

--- public/js/app-init.js
+++ public/js/app-init.js
@@ -7,3 +7,25 @@ window.addEventListener('load', () => {
     }, 3000);
   }
 });
+
+function showUpdateNotification() {
+    const notification = document.getElementById('update-notification');
+    const reloadButton = document.getElementById('update-reload-button');
+    if (notification && reloadButton) {
+        notification.style.display = 'flex';
+        reloadButton.addEventListener('click', () => {
+            // This tells the new service worker to take control and reload the page
+            window.location.reload();
+        });
+    }
+}
+
+if ('serviceWorker' in navigator && window.workbox) {
+    const wb = new window.workbox.Workbox('/sw.js');
+
+    wb.addEventListener('waiting', (event) => {
+        showUpdateNotification();
+    });
+
+    wb.register();
+}
